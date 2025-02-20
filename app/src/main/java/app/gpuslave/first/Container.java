e99a4a1b5172da881f0de700c96776ff91cf9867
Aleksei Nemtsev <shimanoee@yandex.ru>
2025-02-20 13:15:26 +0300

    add javadoc documentation

--- app/src/main/java/app/gpuslave/first/Container.java
+++ app/src/main/java/app/gpuslave/first/Container.java
@@ -1,16 +1,44 @@
 package app.gpuslave.first;
 
+/**
+ * Класс {@code Container} представляет собой обобщенный контейнер для хранения
+ * элементов любого типа.
+ * Он реализован на основе массива и позволяет добавлять, получать, удалять
+ * элементы, а также
+ * узнавать текущий размер контейнера.
+ *
+ * @param <T> Тип элементов, хранящихся в контейнере.
+ */
 public class Container<T> {
+  /**
+   * Массив для хранения элементов контейнера.
+   */
   private T[] elements;
+  /**
+   * Текущий размер контейнера (количество элементов в нем).
+   */
   private int size;
+  /**
+   * Размер контейнера по умолчанию при создании.
+   */
   private static final int DEFAULT_CAPACITY = 10;
 
+  /**
+   * Создает новый пустой контейнер с размером по умолчанию.
+   */
   @SuppressWarnings("unchecked")
   public Container() {
     elements = (T[]) new Object[DEFAULT_CAPACITY];
     size = 0;
   }
 
+  /**
+   * Добавляет элемент в конец контейнера.
+   * Если контейнер заполнен, происходит автоматическое увеличение размера
+   * массива.
+   *
+   * @param element Элемент, который необходимо добавить.
+   */
   public void add(T element) {
     if (size == elements.length) {
       grow();
@@ -18,6 +46,14 @@ public class Container<T> {
     elements[size++] = element;
   }
 
+  /**
+   * Возвращает элемент из контейнера по указанному индексу.
+   *
+   * @param index Индекс элемента, который необходимо получить.
+   * @return Элемент, находящийся по указанному индексу.
+   * @throws IndexOutOfBoundsException Если индекс находится вне допустимого
+   *                                   диапазона.
+   */
   public T get(int index) {
     if (index < 0 || index >= size) {
       throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
@@ -25,6 +61,14 @@ public class Container<T> {
     return elements[index];
   }
 
+  /**
+   * Удаляет элемент из контейнера по указанному индексу.
+   * После удаления элементы, находящиеся после удаленного, сдвигаются влево.
+   *
+   * @param index Индекс элемента, который необходимо удалить.
+   * @throws IndexOutOfBoundsException Если индекс находится вне допустимого
+   *                                   диапазона.
+   */
   public void remove(int index) {
     if (index < 0 || index >= size) {
       throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
@@ -36,10 +80,18 @@ public class Container<T> {
     elements[--size] = null;
   }
 
+  /**
+   * Возвращает текущий размер контейнера (количество элементов в нем).
+   *
+   * @return Текущий размер контейнера.
+   */
   public int size() {
     return size;
   }
 
+  /**
+   * Увеличивает размер массива, используемого для хранения элементов, в два раза.
+   */
   @SuppressWarnings("unchecked")
   private void grow() {
     int newCapacity = elements.length * 2;
@@ -48,6 +100,10 @@ public class Container<T> {
     elements = newElements;
   }
 
+  /**
+   * Выводит в консоль содержимое контейнера в формате [element1, element2, ...,
+   * elementN].
+   */
   public void print() {
     System.out.print("[");
     for (int i = 0; i < size; i++) {
