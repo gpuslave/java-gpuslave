63f548ec3cd39290867db8dc93fd07f53b1c358b
Aleksei Nemtsev <shimanoee@yandex.ru>
2025-02-20 13:15:26 +0300

    add javadoc documentation

--- app/src/main/java/app/gpuslave/first/First.java
+++ app/src/main/java/app/gpuslave/first/First.java
@@ -1,12 +1,28 @@
 package app.gpuslave.first;
 
+/**
+ * Класс {@code First} - это основной класс приложения, демонстрирующий работу с
+ * классом {@link Container}.
+ */
 public class First {
 
+  /**
+   * Возвращает строку "Hello, world!".
+   *
+   * @return Строка "Hello, world!".
+   */
   public String Hello() {
     String str = "Hello, world!";
     return str;
   }
 
+  /**
+   * Основной метод приложения.
+   * Выводит в консоль строку "Hello, world!" и демонстрирует использование класса
+   * {@link Container}.
+   *
+   * @param args Аргументы командной строки (не используются).
+   */
   public static void main(String[] args) {
     First firstInstance = new First();
     System.out.println(firstInstance.Hello());
