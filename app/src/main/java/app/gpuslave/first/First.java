d3f7595570f15cd147e36fd126191e9e95604bca
alex <shimanoee@yandex.ru>
2025-04-13 16:41:28 +0300

    enhance javadoc for First

--- app/src/main/java/app/gpuslave/first/First.java
+++ app/src/main/java/app/gpuslave/first/First.java
@@ -1,8 +1,19 @@
 package app.gpuslave.first;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
- * Класс {@code First} - это основной класс приложения, демонстрирующий работу с
- * классом {@link Container}.
+ * Класс {@code First} - это основной класс приложения, демонстрирующий работу
+ * с классами из всех лабораторных работ:
+ * <ul>
+ * <li>Первая лабораторная: использование класса {@link Container} для хранения
+ * элементов</li>
+ * <li>Вторая лабораторная: использование класса {@link ExpressionEvaluator} для
+ * вычисления математических выражений</li>
+ * <li>Третья лабораторная: использование класса {@link Compare} для сравнения
+ * производительности коллекций</li>
+ * </ul>
  */
 public class First {
 
@@ -18,12 +29,19 @@ public class First {
 
   /**
    * Основной метод приложения.
-   * Выводит в консоль строку "Hello, world!" и демонстрирует использование класса
-   * {@link Container}.
+   * Последовательно демонстрирует работу всех трех лабораторных работ:
+   * <ul>
+   * <li>Первая лабораторная: Класс {@link Container} для хранения элементов</li>
+   * <li>Вторая лабораторная: Класс {@link ExpressionEvaluator} для вычисления
+   * математических выражений</li>
+   * <li>Третья лабораторная: Класс {@link Compare} для сравнения
+   * производительности коллекций</li>
+   * </ul>
    *
    * @param args Аргументы командной строки (не используются).
    */
   public static void main(String[] args) {
+
     System.out.println("----------FIRST--LAB------------");
     First firstInstance = new First();
     System.out.println(firstInstance.Hello());
@@ -37,5 +55,22 @@ public class First {
 
     System.out.println(eval.evaluate("(1+a+C) + (DOUBLE)"));
     System.out.println(eval.evaluate("1+(2)*(300)"));
+
+    System.out.println("----------THIRD--LAB------------");
+    Compare cmpr = new Compare();
+    System.out.println("Сравнение производительности ArrayList и LinkedList");
+    System.out.println("Количество операций для каждого метода: " + Compare.ITERATIONS);
+    System.out.println("Размер коллекций: " + Compare.LIST_SIZE);
+    System.out.println("\n");
+
+    ArrayList<Integer> arrayList = new ArrayList<>();
+    LinkedList<Integer> linkedList = new LinkedList<>();
+
+    for (int i = 0; i < Compare.LIST_SIZE; i++) {
+      arrayList.add(i);
+      linkedList.add(i);
+    }
+
+    cmpr.runTests(arrayList, linkedList);
   }
 }
