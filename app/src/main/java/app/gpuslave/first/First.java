14220b0a64fae9f4e8693c6cf69066df1bfc2b2d
alex <shimanoee@yandex.ru>
2025-04-13 17:52:18 +0300

    add CSV class usage

--- app/src/main/java/app/gpuslave/first/First.java
+++ app/src/main/java/app/gpuslave/first/First.java
@@ -2,6 +2,7 @@ package app.gpuslave.first;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Класс {@code First} - это основной класс приложения, демонстрирующий работу
@@ -13,9 +14,20 @@ import java.util.LinkedList;
  * вычисления математических выражений</li>
  * <li>Третья лабораторная: использование класса {@link Compare} для сравнения
  * производительности коллекций</li>
+ * <li>Четвертая лабораторная: использование класса {@link CSV} для чтения и
+ * обработки
+ * данных из CSV файла</li>
  * </ul>
  */
 public class First {
+
+  /**
+   * Конструктор по умолчанию для класса First.
+   * Создает новый экземпляр класса без инициализации полей.
+   */
+  public First() {
+    // Пустой конструктор
+  }
 
   /**
    * Возвращает строку "Hello, world!".
@@ -29,13 +41,15 @@ public class First {
 
   /**
    * Основной метод приложения.
-   * Последовательно демонстрирует работу всех трех лабораторных работ:
+   * Последовательно демонстрирует работу всех четырех лабораторных работ:
    * <ul>
    * <li>Первая лабораторная: Класс {@link Container} для хранения элементов</li>
    * <li>Вторая лабораторная: Класс {@link ExpressionEvaluator} для вычисления
    * математических выражений</li>
    * <li>Третья лабораторная: Класс {@link Compare} для сравнения
    * производительности коллекций</li>
+   * <li>Четвертая лабораторная: Класс {@link CSV} для чтения и обработки
+   * данных из CSV файла</li>
    * </ul>
    *
    * @param args Аргументы командной строки (не используются).
@@ -72,5 +86,37 @@ public class First {
     }
 
     cmpr.runTests(arrayList, linkedList);
+
+    System.out.println("----------FOURTH-LAB------------");
+    System.out.println("Чтение и обработка данных из CSV файла");
+
+    String csvFilePath = "/home/gpuslave/pet/java-gpuslave/foreign_names.csv";
+    List<CSV.Person> people = CSV.parseCSV(csvFilePath, ';');
+    System.out.println("Загружено " + people.size() + " записей из CSV файла");
+
+    System.out.println("Примеры записей:");
+    for (int i = 0; i < Math.min(5, people.size()); i++) {
+      CSV.Person person = people.get(i);
+      System.out.println((i + 1) + ". " + person.getName() +
+          " (" + person.getGender() + "), " +
+          "отдел: " + person.getDivision().getTitle() +
+          ", зарплата: " + person.getSalary());
+    }
+
+    System.out.println("\nПример группировки по отделам:");
+    java.util.Map<String, Integer> divisionCounts = new java.util.HashMap<>();
+
+    for (CSV.Person person : people) {
+      String division = person.getDivision().getTitle();
+      divisionCounts.put(division, divisionCounts.getOrDefault(division, 0) + 1);
+    }
+
+    int count = 0;
+    for (java.util.Map.Entry<String, Integer> entry : divisionCounts.entrySet()) {
+      System.out.println("Отдел " + entry.getKey() + ": " + entry.getValue() + " сотрудников");
+      count++;
+      if (count >= 5)
+        break;
+    }
   }
 }
